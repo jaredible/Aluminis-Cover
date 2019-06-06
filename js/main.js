a79a868e4025c0f338eb5a22078037ac75d1dda0
Jared Diehl <jareddiehl96@gmail.com>
2019-06-06 07:24:18 -0500

    Trying to add images over images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-//$.firefly({
-//  minPixel: 1,
-//  maxPixel: 3,
-//  total: 25
-//});
+$.firefly({
+  minPixel: 1,
+  maxPixel: 3,
+  total: 25
+});
+
+$(function() {
+  $('a[href*=\\#]').on('click', function(e) {
+    e.preventDefault();
+    console.log('here');
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'swing');
+  });
+});
